e5fcf30abb4a4e9d1042156c3985a1557ee2f02e
Chad Christensen <chadthecoder@gmail.com>
2016-01-21 21:23:48 -0500

    Fix track duration

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ Track.create(title: 'Gangsta Party',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bgangstaparty2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bgangstaparty.mp3',
              price: 100,
-             time: 330,
+             time: 243,
              producer: producer1)
 
 Track.create(title: 'My First Million',
@@ -35,7 +35,7 @@ Track.create(title: 'My First Million',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bmyfirstmillion2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bmyfirstmillion.mp3',
              price: 500,
-             time: 430,
+             time: 351,
              producer: producer1)
 
 Track.create(title: 'Paper Planes',
@@ -43,7 +43,7 @@ Track.create(title: 'Paper Planes',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bpaperplanes2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bpaperplanes.mp3',
              price: 600,
-             time: 345,
+             time: 261,
              producer: producer1)
 
 Track.create(title: 'Still Cold',
@@ -51,7 +51,7 @@ Track.create(title: 'Still Cold',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bstillcold2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/kontrabandz%2Bstillcold.mp3',
              price: 1000,
-             time: 300,
+             time: 278,
              producer: producer1)
 
 Track.create(title: 'Empty Streets',
@@ -59,7 +59,7 @@ Track.create(title: 'Empty Streets',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bemptystreets2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bemptystreets.mp3',
              price: 20000,
-             time: 300,
+             time: 267,
              producer: producer2)
 
 Track.create(title: 'Fade to Black',
@@ -67,7 +67,7 @@ Track.create(title: 'Fade to Black',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bfadetoblack2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bfadetoblack.mp3',
              price: 5000,
-             time: 300,
+             time: 267,
              producer: producer2)
 
 Track.create(title: 'The Light',
@@ -75,7 +75,7 @@ Track.create(title: 'The Light',
              thumb_img: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bthelight2.jpg',
              audio_url: 'https://s3-us-west-2.amazonaws.com/beats-app/dreamlifebeats%2Bthelight.mp3',
              price: 40,
-             time: 300,
+             time: 277,
              producer: producer2)
 
 
